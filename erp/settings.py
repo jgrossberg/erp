3b0f3f4826dbaca7716c647d0377e24a567f6b0a
Jonah Grossberg <jonahag@gmail.com>
2019-05-21 22:40:14 -0400

    Updated the JE react view

--- erp/settings.py
+++ erp/settings.py
@@ -128,4 +128,8 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
-CORS_ORIGIN_WHITELIST = ['http://localhost:3000']
+CORS_ORIGIN_WHITELIST = [
+    'http://localhost:3000',
+]
+
+CORS_ORIGIN_ALLOW_ALL = True
