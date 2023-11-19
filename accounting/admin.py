c17f1c6a66f9a0729afe3d9964d2412d0e5730a9
Jonah Grossberg <jonahag@gmail.com>
2023-11-19 18:36:36 -0500

    add journal entry templates to backend

--- accounting/admin.py
+++ accounting/admin.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
 
-from .models import Account, Entry
+from .models import Account, Entry, JournalEntryTemplate
 
 # Register your models here.
 admin.site.register(Account)
 admin.site.register(Entry)
+admin.site.register(JournalEntryTemplate)
