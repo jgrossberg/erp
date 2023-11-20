a51750ce7f0093a40b97a36cf53fded1d02e271a
Jonah Grossberg <jonahag@gmail.com>
2023-11-19 20:39:56 -0500

    add selector and fetches

--- frontend/src/containers/JournalEntryView.js
+++ frontend/src/containers/JournalEntryView.js
@@ -2,10 +2,35 @@ import React from 'react';
 
 import JournalEntryForm from '../components/JournalEntryForm';
 
+
+const TemplateSelector = (props) => {
+    const handleTemplateSelect = (event) => {
+        console.log(event.target.value)
+        // dispatchEvent(new Event('templateSelect'))
+    }
+
+    let templates = Array.from(props.templates).map(item => {
+        return (
+          <option key={item.name}>
+            {item.name}
+          </option>
+        )
+      })
+
+    return (
+        <div>
+            <select onChange={handleTemplateSelect}>
+                {templates}
+            </select>
+        </div>
+    )
+}
+
 function JournalEntry() {
         return (
             <div>
-                <JournalEntryForm/>
+                <TemplateSelector templates={templates} onChange={this.handleTemplateSelect}/>
+                <JournalEntryForm debits={debits} credits={credits}/>
             </div>
         )
 }
