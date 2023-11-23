d48b5c2e1721d08e98446e91c8ac92d276b0b64f
Jonah Grossberg <jonahag@gmail.com>
2023-11-22 23:53:14 -0500

    better organize fe components, fetch je templates from JournalEntryView

--- frontend/src/containers/JournalEntryView.js
+++ frontend/src/containers/JournalEntryView.js
@@ -1,13 +1,24 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 
 import JournalEntryForm from "../components/JournalEntryForm";
 
 function JournalEntry() {
+
+  const [templates, setTemplates] = useState([]);
+  
+  useEffect(() => {
+    fetch(`http://127.0.0.1:8000/templates/`)
+      .then((response) => response.json())
+      .then((data) => setTemplates(data));
+
+    }, [])
+    console.log(templates)
   return (
     <div>
       <JournalEntryForm />
     </div>
   );
+
 }
 
 export default JournalEntry;
